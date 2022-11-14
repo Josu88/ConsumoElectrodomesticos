cd0005d18f9f6ea76a98276437a6736fbca24410
Josue <jri53268805@gmail.com>
2022-11-14 23:54:08 +0100

    Creado el fetch del js y actualizado el html

--- js/luz.js
+++ js/luz.js
@@ -1,1 +1,42 @@
 "use strict";
+
+//Seleccionamos el cuadro de texto del formulario del html
+const form = document.querySelector("form.search");
+const app = document.querySelector("section.app");
+
+//FUNCIONES
+
+//Arrow funtion para que aparezca un parrafo con el mensaje de error
+const writeMessage = (message) => {
+  app.innerHTML = `<p>${message}</p>`;
+};
+
+//Arrow funtion para sacar un array de los datos de la api de la luz
+const writeInfLuz = (conLuz) => {
+  //Convertir a array el JSON
+  let arrayLuz = JSON.parse(conLuz);
+  console.log(arrayLuz["00-01"]);
+  console.log(arrayLuz);
+};
+
+const doSearch = async (e) => {
+  e.preventDefault();
+  try {
+    const url = `https://api.allorigins.win/get?url=https://api.preciodelaluz.org/v1/prices/all?zone=PCB`;
+
+    writeMessage("Cargando Datos....");
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    if (response.ok) {
+      writeInfLuz(data.contents);
+    } else {
+      writeMessage("Hubo un error haciendo la petición");
+    }
+  } catch (error) {
+    writeMessage(error.message);
+  }
+};
+form.addEventListener("submit", doSearch);
