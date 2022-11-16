7d12ef84fa9aa28d4137346b048a7f0f709e995f
Josue <jri53268805@gmail.com>
2022-11-16 20:28:38 +0100

    javascript con fetch y setInterval completos

--- js/luz.js
+++ js/luz.js
@@ -1,8 +1,12 @@
 "use strict";
 
+//Definimos las variables a usar
+let counter = 1;
+
 //Seleccionamos el cuadro de texto del formulario del html
 const form = document.querySelector("form.search");
 const app = document.querySelector("section.app");
+const boton = form.querySelector("button.btnForm");
 
 //FUNCIONES
 
@@ -11,32 +15,61 @@ const writeMessage = (message) => {
   app.innerHTML = `<p>${message}</p>`;
 };
 
-//Arrow funtion para sacar un array de los datos de la api de la luz
-const writeInfLuz = (conLuz) => {
-  //Convertir a array el JSON
-  let arrayLuz = JSON.parse(conLuz);
-  console.log(arrayLuz["00-01"]);
-  console.log(arrayLuz);
+//Arrow funtion para contar las pulsaciones de un boton del formulario
+let writeCont = () => {
+  let Num = counter++;
+  return Num;
 };
 
-const doSearch = async (e) => {
-  e.preventDefault();
+//Arrow Function para sacar datos de la api y mandarselos a otra funcion para mostrarlos
+const doSearch = async () => {
   try {
+    //creamos el fetch para conexion con la api
     const url = `https://api.allorigins.win/get?url=https://api.preciodelaluz.org/v1/prices/all?zone=PCB`;
-
-    writeMessage("Cargando Datos....");
 
     const response = await fetch(url);
 
     const data = await response.json();
 
+    //Sacamos un mensaje por el parafo
+    writeMessage("Cargando Datos....");
+
+    //Comprobamos si la respuesta esta bien
     if (response.ok) {
-      writeInfLuz(data.contents);
+      //Convertir a array el JSON
+      let arrayLuz = JSON.parse(data.contents);
+      console.log(arrayLuz["00-01"]);
+      console.log(arrayLuz);
     } else {
+      //Si no sacamos por consola un mensaje de error
       writeMessage("Hubo un error haciendo la petición");
     }
   } catch (error) {
+    //si falla el catch sacamos por consola un mensaje de error
     writeMessage(error.message);
   }
 };
-form.addEventListener("submit", doSearch);
+
+const interval = async (e) => {
+  e.preventDefault();
+
+  //Definimos las variables a usar en la funcion
+  let miliSegs = 0;
+
+  //Comprobamos cuantas veces pulsamos el boton
+  if (writeCont() < 2) {
+    miliSegs = 1000;
+  } else {
+    miliSegs = 300000;
+  }
+
+  console.log(miliSegs);
+
+  //Si esta bien ejecutamos la funcion con el setinterval
+  const SetIn = setInterval(doSearch, miliSegs);
+
+  return SetIn;
+};
+
+//Añadimos el event listener al boton del formulario
+boton.addEventListener("click", interval);
