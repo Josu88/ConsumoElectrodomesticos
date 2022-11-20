845488863ef23fc9a3bcd04ac61aa2be58bac158
Daniiotero <daniel.otero.perez.15@gmail.com>
2022-11-20 20:53:52 +0100

    Añadida funcion que saca la mejor y peor hora
    para gastar luz y la muestra en el DOM

--- js/precio.js
+++ js/precio.js
@@ -2,7 +2,8 @@
 const watts = (arrayLuz) => {
   let actualHour = Hours();
   actualHour += "-"; //Le añado un guión a la hora generada para compararlo con arrayLuz
-  console.log(actualHour);
+
+  //Obtenemos el precio de la hora actual
   for (let i in arrayLuz) {
     if (arrayLuz[i].hour.includes(actualHour)) {
       return arrayLuz[i].price;
@@ -13,11 +14,12 @@ const watts = (arrayLuz) => {
 //Función que guarda en el array devices el Precio de la luz que consumen los distintos electrodomésticos en ese momento del día
 const calcPrices = (arrayLuz, devices) => {
   const actualPriceWatts = watts(arrayLuz);
+
+  //Calculamos los precios multiplicando los Watts con el consumo de los electrodomésticos
   for (let i in devices) {
     devices[i].price = actualPriceWatts * devices[i].consumption;
-    devices[i].price = devices[i].price.toFixed(2) + "€"; //Redondeamos el resultado de la operación anterior a dos decimales y le añadimos el símbolo de euro
+    devices[i].price = devices[i].price.toFixed(2); //Redondeamos el resultado de la operación anterior a dos decimales
   }
-  console.log(devices);
 };
 
 //Función que devuelve la hora actual
@@ -28,6 +30,43 @@ const Hours = () => {
   return now;
 };
 
-//const betterAndWorstHour = (arrayLuz) => {};
+//Función para sacar la mejor y la peor hora para gastar luz
+const betterAndWorstHour = (arrayLuz) => {
+  //Declarar variables
+  const allPriceArray = [];
+  const allHoursArray = [];
+  let betterHour = "";
+  let worstHour = "";
+
+  //Adaptamos la info de la Api para convertirlo en un array y poder iterar
+  for (let i in arrayLuz) {
+    allPriceArray[i] = arrayLuz[i].price;
+    allHoursArray[i] = arrayLuz[i].hour;
+  }
+  const values = Object.values(allPriceArray);
+  const hours = Object.values(allHoursArray);
+
+  //Comparamos los precios para obtener el mayor y el menor
+  let max = values.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+  let min = values.reduce(function (a, b) {
+    return Math.min(a, b);
+  });
+
+  //Comparamos los precios para obtener la hora exacta
+  for (let i = 0; i < 24; i++) {
+    if (values[i] === max) {
+      worstHour = hours[i];
+    }
+    if (values[i] === min) {
+      betterHour = hours[i];
+    }
+  }
+
+  return [betterHour, worstHour];
+};
 
 export default calcPrices;
+export { Hours };
+export { betterAndWorstHour };
